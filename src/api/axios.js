358a3c2300a317b132bbc0a0df7a5b9466e63f25
victor <victormfr2003@gmail.com>
2023-09-28 05:28:13 -0400

    Linking to Backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://school-rest-api-nu.vercel.app/',
     withCredentials: true
 })
