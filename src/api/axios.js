e3a40e015bfdda0cdd102600b6ab586c8c56cea5
VictorMfr <89408124+VictorMfr@users.noreply.github.com>
2024-04-26 05:44:56 -0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://school-rest-api-nu.vercel.app',
     withCredentials: true
 })
